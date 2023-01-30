5b2f29ead21b002f908157a8fe3402257e2a13da
Eduardo Falvo <dudu.falvo@gmail.com>
2023-01-30 17:55:07 +0000

    database admin permissions

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DietaryRestriction, Ingredient, Recipe
 
 # Register your models here.
+
+# admin.site.register(User)
+admin.site.register(DietaryRestriction)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
